c8047022c36c65e262d914bd8feab5f8fd562814
ramji013 <ramprm@gmail.com>
2019-08-23 03:56:24 +0530

    custom media player added

--- script.js
+++ script.js
@@ -11,17 +11,17 @@
 	var volumePlus = document.getElementById("volume-plus");
 	var volumeMinus = document.getElementById("volume-minus");
 	var reload = document.getElementById("video-reload");
-	var seekBar = document.getElementById("seek-bar");
+	var progressBar = document.getElementById("progressBar");
+	var likeCount = document.getElementById("like");
+	var unLikeCount = document.getElementById("unlike");
+	var progress = document.getElementById("progress");
+	
 
 	// Event listener for the play/pause button
 	playButton.addEventListener("click", function() {
 		if (video.paused == true) {
 			// Play the video
 			video.play();
-			
-			// Update the button text to 'Pause'
-			//playButton.innerHTML = "Pause";
-			
 			document.getElementById("play").disabled = true;
 			document.getElementById("pause").disabled = false;
 		} 
@@ -33,10 +33,6 @@
 		if (video.paused == false) {
 			// Play the video
 			video.pause();
-			
-
-			// Update the button text to 'Pause'
-			//playButton.innerHTML = "Pause";
 			document.getElementById("play").disabled = false;
 			document.getElementById("pause").disabled = true;
 		} 
@@ -64,10 +60,7 @@
 	// Event listener for the volume bar
 	reload.addEventListener("click", function() {
 		// Update the video volume
-			//video.currentTime = 0;
-			//video.reload = this.video.;
-			//video.reload = true;
-	
+			video.currentTime = 0;
 	});
 
 	// Event listener for the mute button
@@ -75,49 +68,27 @@
 		if (video.muted == false) {
 			// Mute the video
 			video.muted = true;
-
-			// Update the button text
-			//muteButton.innerHTML = "Unmute";
 		} else {
 			// Unmute the video
 			video.muted = false;
-
-			// Update the button text
-			//muteButton.innerHTML = "Mute";
 		}
 	});
 
 
-	// Event listener for the seek bar
-	seekBar.addEventListener("change", function() {
-		// Calculate the new time
-		var time = video.duration * (seekBar.value / 100);
+	video.addEventListener('loadedmetadata', function() {
+		progress.setAttribute('max', video.duration);
+		if (video.paused == true) {
+			document.getElementById("play").disabled = false;
+			document.getElementById("pause").disabled = true;
+		}
+	 });
 
-		// Update the video time
-		video.currentTime = time;
-	});
+	 video.addEventListener('timeupdate', function() {
+		if (!progress.getAttribute('max')) progress.setAttribute('max', video.duration);
+	 		 progress.value = video.currentTime;
+  			 progressBar.style.width = Math.floor((video.currentTime / video.duration) * 100) + '%';
+	 });
 
-	
-	// Update the seek bar as the video plays
-	video.addEventListener("timeupdate", function() {
-		// Calculate the slider value
-		var value = (100 / video.duration) * video.currentTime;
-
-		// Update the slider value
-		seekBar.value = value;
-	});
-
-	// Pause the video when the seek handle is being dragged
-	seekBar.addEventListener("mousedown", function() {
-		video.pause();
-	});
-
-	// Play the video when the seek handle is dropped
-	seekBar.addEventListener("mouseup", function() {
-		video.play();
-	});
-
-	
 	// Event listener for the play/pause button
 	pauseButton.addEventListener("click", function() {
 		
@@ -132,3 +103,57 @@
 			document.getElementById("pause").disabled = true;
 		} 
 	});
+
+	likeCount.addEventListener("click", function(){
+		var count = localStorage.getItem("like");
+		if(count==null){
+			count = 0;
+		}else{
+			count = parseInt(count);
+		}
+		count += 1;
+		localStorage.setItem("like", count);
+		document.getElementById("like-count").innerText = count;
+	})
+
+	unLikeCount.addEventListener("click", function(){
+		var unLikeCount = localStorage.getItem("unLike");
+		var likeCount = localStorage.getItem("like");
+		if(likeCount!=null && likeCount>0){
+			if(unLikeCount==null){
+				unLikeCount = 0;
+			}else{
+				unLikeCount = parseInt(unLikeCount);
+			}
+			unLikeCount += 1;
+			localStorage.setItem("unLike", unLikeCount);
+			likeCount -= 1;
+			localStorage.setItem("like", likeCount);
+			document.getElementById("unlike-count").innerText = unLikeCount;
+			document.getElementById("like-count").innerText = likeCount;
+	}
+		
+	})
+
+
+var video_player = document.getElementById("video_player"),
+links = video_player.getElementsByTagName('a');
+for (var i=0; i<links.length; i++) {
+	links[i].onclick = handler;
+}
+
+
+function handler(e) {
+	e.preventDefault();
+	videotarget = this.getAttribute("href");
+	filename = videotarget.substr(0, videotarget.lastIndexOf('.')) || videotarget;
+	video = document.querySelector("#video_player video");
+	video.removeAttribute("controls");
+	video.removeAttribute("poster");
+	source = document.querySelectorAll("#video_player video source");
+	source[0].src = filename + ".mp4";
+	video.load();
+	video.play();
+	document.getElementById("play").disabled = true;
+	document.getElementById("pause").disabled = false;
+}
